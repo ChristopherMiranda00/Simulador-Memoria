190e4e12a6a55b42108ba109cad032052e552b1b
Christopher Miranda <chrisMac@ChrisMac.local>
2020-07-30 14:37:37 -0500

    Comentarios analizador

--- analizador.py
+++ analizador.py
@@ -23,7 +23,7 @@ def instrucciones():
                 else:
                     instruccion = [secciones[0]]
                     try:
-                        #Por cada palabra convierte los ultimos 3 argumentos en enteros ya que se pasan como string y los mete en una lista
+                        #Se intenta por cada palabra convierte los ultimos 3 argumentos en enteros ya que se pasan como string y los mete en una lista
                         instruccion.append(int(secciones[1])) 
                         instruccion.append(int(secciones[2]))
                         instruccion.append(int(secciones[3]))
@@ -36,44 +36,54 @@ def instrucciones():
                         exit()
             
             elif secciones[0] == 'P':
+                #número de argumentos P "#asginar num bytes" "#num proceso"
                 if(len(secciones) != 3):
                     print('ERROR: Numero incorrecto de argumentos ')
                 else:
                     instruccion = [secciones[0]]
-                    try:
+                    try: 
+                        #Se intenta por cada palabra convierte los ultimos 2 argumentos en enteros ya que se pasan como string y los mete en una lista
                         instruccion.append(int(secciones[1])) 
                         instruccion.append(int(secciones[2]))
-                        listaDeInstrucciones.append(instruccion)
+                        listaDeInstrucciones.append(instruccion) #Añade la instruccion a la lista de instrucciones
                     except ValueError:
                         print("Uno de los argumentos ingresados es incorrecto")
                         exit()
             
             elif secciones[0] == 'L':
+                #L solo puede tener 2 palabras "L "#proceso p" "
                 if(len(secciones) != 2):
                     print('ERROR: Numero incorrecto de argumentos')
                 else:
                     instruccion = [secciones[0]]
                     try:
+                        #Se intenta por cada palabra convierte el último  argumento en entero ya que se pasa como string y lo mete en una lista
                         instruccion.append(int(secciones[1]))
-                        listaDeInstrucciones.append(instruccion)
+                        listaDeInstrucciones.append(instruccion) #Añade la instruccion a la lista de instrucciones
                     except ValueError:
                         print("Uno de los argumentos ingresados es incorrecto")
                         exit()
 
-            elif secciones[0] == 'F':
+
+            elif secciones[0] == 'F': #última línea de un conjunto de solicitudes
                 instruccion = [secciones[0]]
+                #Añade la instruccion a la lista de instrucciones
                 listaDeInstrucciones.append(instruccion)
 
-            elif secciones[0] == 'E':
+            elif secciones[0] == 'E': #Exit, últime línea del archivo 
                 instruccion = [secciones[0]]
+                #Añade la instruccion a la lista de instrucciones
                 listaDeInstrucciones.append(instruccion)
 
-            elif secciones[0] == 'C':
+            elif secciones[0] == 'C': #Comentarios
                 instruccion = [secciones[0]]
+                #Junta los comentarios que posteriormente serán despledados
                 instruccion.append(' '.join(secciones[1::]))
+                #Añade la instruccion a la lista de instrucciones
                 listaDeInstrucciones.append(instruccion)
 
             else:
+                #En caso que la instrución sea diferentes a las anteriores =ERROR
                 print('Error: Ingreso una instruccion inexistente')
 
         return listaDeInstrucciones
