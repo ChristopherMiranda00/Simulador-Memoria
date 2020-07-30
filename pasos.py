68e9859c995a573a6bb9280e70c76ab7d7721a2a
Miguel Alejandro Hernandez Garcia <54487427+SilverGud@users.noreply.github.com>
2020-07-30 14:37:07 -0500

    Add files via upload

--- pasos.py
+++ pasos.py
@@ -44,7 +44,31 @@ def swap(paginaNueva, procesosNuevo, siguienteFrame):
     
     print("La Pagina: ", paginaAnterior," del proceso: ",procesoAnterior," haciendo swapping al marco: ",math.floor(verificadorFrameLibreEnSwap/tamañoDePaginas), "del área de swapping")
 
+    cargarPaginaSwap(verificadorFrameLibreEnSwap, procesoAnterior, paginaAnterior)
 
+    if procesoAnterior not in PaginasMenejoSwapp:
+        PaginasMenejoSwapp[procesoAnterior] = {}
+
+    PaginasMenejoSwapp[procesoAnterior][paginaAnterior] = verificadorFrameLibreEnSwap
+
+    del PaginasMenejoSwapp[procesoAnterior][paginaAnterior]
+
+    cargarPaginaFrame(siguienteFrame, procesosNuevo, paginaNueva)
+    PaginasMenejoSwapp[procesosNuevo][paginaNueva] = siguienteFrame
+    tiempoMedida += 20
+
+    return True
+
+def cargarPaginaSwap(frameLibre, procesoAnterior, paginaAnterior):
+    val = None if procesoAnterior == None and paginaAnterior == None else [procesoAnterior,paginaAnterior]
+
+    for i in range(0, 16):
+        areaSwapping[frameLibre + i] = val
+
+def cargarPaginaFrame(pagina, procesosNuevo, paginaNueva):
+    val = None if procesosNuevo == None and paginaNueva == None else [procesosNuevo, paginaNueva]
+    for i in range(0, 16):
+        memory[pagina + i] = val
 
 def P(n, p): #Paso[1] = bytes a asignar, Paso[2] = proceso
     global tiempoMedida, swapsTotales, fallosDePaginaTotales
@@ -66,8 +90,18 @@ def P(n, p): #Paso[1] = bytes a asignar, Paso[2] = proceso
     procesosDePagina[p]["tiempoInicio"] = tiempoMedida
     paginaActual = 0
     manejoDeFramesVacios = 0
+    frames = []
 
     while paginaActual < numeroPaginas:
         if paginaActual < numeroPaginas and manejoDeFramesVacios >= 2048:
             siguienteFrame = escojeFrameParaSwap()
             cambio = swap(paginaActual, p, siguienteFrame)
+
+            if not cambio:
+                return
+            
+            frames.append(math.floor(siguienteFrame/16))
+            swapsTotales += 1
+            paginaActual += 1
+
+    
